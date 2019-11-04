fd294ec9d9f768c08ffe4dce7f62de42a36be649
Nabil Madih <nabilmadih.a@gmail.com>
2019-11-04 19:15:00 +0800

    Add project files.

---
+++ swarm1/cParticle.h
@@ -0,0 +1,28 @@
+#pragma once
+// Filename: cParticle.h
+
+#ifndef C_PARTICLE_H
+#define C_PARTICLE_H
+
+#include "swarm1.h"
+
+class cParticle
+{
+
+private:
+	int     mData[MAX_INPUTS];
+	int     mpBest;
+	float   mVelocity;
+
+public:
+	cParticle();
+	int     getData(int index) const;
+	void    setData(int index, int value);
+	int     getpBest() const;
+	void    setpBest(int value);
+	float   getVelocity() const;
+	void    setVelocity(float value);
+
+}; // end cParticle class.
+
+#endif
