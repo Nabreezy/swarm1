29d8a98d8b548b5b0248ea626d395cb2d87eba4b
Iohann Bucaille <iohann.bucaille@gmail.com>
2019-11-05 14:30:17 +0800

    named functions

--- swarm1/functions.h
+++ swarm1/functions.h
@@ -2,18 +2,18 @@
 #define C_FUNCTIONS_H
 #include "swarm1.h"
 int test(cParticle particle);
-int f1(cParticle particle);
-int f2(cParticle particle);
-int f3(cParticle particle); // no sense
-int f4(cParticle particle);
-int f5(cParticle particle);
-int f6(cParticle particle);
-int f7(cParticle particle); 
-int f8(cParticle particle); 
+int f1(cParticle particle); // Sphere
+int f2(cParticle particle); // Schwefel's 2.22
+int f3(cParticle particle); // Schwefel's 1.20
+int f4(cParticle particle); // Schwefel's 2.21
+int f5(cParticle particle); // Rosenbrock
+int f6(cParticle particle); // Step
+int f7(cParticle particle); // Quartic Noise
+int f8(cParticle particle);
 int f9(cParticle particle);
-int f10(cParticle particle);
-int f11(cParticle particle);
-int f12(cParticle particle); // y not define
+int f10(cParticle particle); // Ackley
+int f11(cParticle particle); // look's like Griewank
+int f12(cParticle particle);
 int f13(cParticle particle);
 int f14(cParticle particle); // a not define
 int f15(cParticle particle); // a, b not define
